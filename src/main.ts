6f51c81c6b92768a18c7a893d16f5e91fe8ef292
Victor <emmyvic98@gmail.com>
2025-03-24 04:39:34 +0100

    big int to json and extended server timeout

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  (BigInt.prototype as any).toJSON = function () {
+    return this.toString();
+  };
   const app = await NestFactory.create(AppModule);
+  const server = app.getHttpServer();
+
+  // ✅ Extend the timeout at the server level
+  server.setTimeout(120000); // 2 minutes (120,000 ms)
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
